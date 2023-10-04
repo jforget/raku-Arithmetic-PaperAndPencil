5019ee2cfedb9d994b7f3c70e6f169f958ce789a
Jean Forget <JFORGET@cpan.org>
2023-10-04 08:12:06 +0200

    A very small bit of explanation. Plus e-lisp functions to type code while in Emacs.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 NAME
 ====
 
-Arithmetic::PaperAndPencil - simulate the computation of arithmetic operations using a pencil and some paper.
+Arithmetic::PaperAndPencil - blah blah blah
 
 SYNOPSIS
 ========
@@ -13,9 +13,7 @@ use Arithmetic::PaperAndPencil;
 DESCRIPTION
 ===========
 
-Arithmetic::PaperAndPencil is a module to simulate operations using
-some paper and a pencil: additions, subtractions, multiplications,
-divisions and even square root computations.
+Arithmetic::PaperAndPencil is ...
 
 AUTHOR
 ======
---
+++ config.el
@@ -0,0 +1,19 @@
+; -*- encoding: utf-8; indent-tabs-mode: nil -*-
+(progn
+(defun md-arrow         () (interactive) (insert "→"))  ; U+2192 rightwards arrow
+(defun md-less-equal    () (interactive) (insert "≤"))  ; U+2264 less-than or equal to
+(defun md-greater-equal () (interactive) (insert "≥"))  ; U+2265 greater-than or equal to
+(defun md-backquotes    () (interactive) (insert "``")   (forward-char -1))
+(defun md-guillemets    () (interactive) (insert "«  »") (forward-char -2))
+(defun md-programme     () (interactive) (insert "\n```\n\n```\n") (forward-line -2))
+(defun raku-add         () (interactive) (insert "☈+"))  ; U+2608 thunderstorm
+(defun raku-times       () (interactive) (insert "☈×"))  ; U+2608 thunderstorm, U+00D7 multiplication sign 
+  (define-key global-map "\C-c-"      'md-arrow)
+  (define-key global-map "\C-c<"      'md-less-equal)
+  (define-key global-map "\C-c>"      'md-greater-equal)
+  (define-key global-map "\C-cè"      'md-backquotes)
+  (define-key global-map "\C-c\C-c<"  'md-guillemets)
+  (define-key global-map "\C-cp"      'md-programme)
+  (define-key global-map "\C-c+"      'raku-add)
+  (define-key global-map "\C-cx"      'raku-times)
+)
